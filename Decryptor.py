b8d2221df4391ac7651ef69fd4924688972ce1a6
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-04-19 11:07:03 +0800

    Decrypting the message

--- Decryptor.py
+++ Decryptor.py
@@ -2,5 +2,16 @@
 encrypted_message = input(str("Please enter an encrypted message"))
 message = ""
 #Check for the character of the message
+for i in range(len(encrypted_message))
 #Decrypt the message
+if encrypted_message[i] == "*":
+    message += "a"
+if encrypted_message[i] == "&":
+    message += "e"
+if encrypted_message[i] == "#":
+    message += "i"
+if encrypted_message[i] == "+":
+    message += "o"
+if encrypted_message[i] == "!":
+    message += "u"
 #Print the message
