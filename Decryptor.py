d78d46ad6efef8daea4e1c01b33a50c307502ee1
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-04-19 10:59:38 +0800

    Ask the user to input an encrypted message

--- Decryptor.py
+++ Decryptor.py
@@ -1,4 +1,6 @@
 #Ask user to input encrypted message
+encrypted_message = input(str("Please enter an encrypted message"))
+message = ""
 #Check for the character of the message
 #Decrypt the message
 #Print the message
